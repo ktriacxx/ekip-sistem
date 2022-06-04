0af2f3574912e34d5c227dc716307cbe20b9d714
Arda <91464589+ktriacxx@users.noreply.github.com>
2022-06-04 11:15:14 +0300

    Update ktria.js

--- ktria.js
+++ ktria.js
@@ -3,3 +3,9 @@ const client = new Client()
 const { Bot, Guild, Ekip } = require("./maveraktria.json") 
 const buttons = require("discord-buttons")
 buttons(client)
+
+client.login(Bot.Token)
+client.on("ready", () => {
+    client.user.setActivity({ name: Bot.Ready })
+    console.log(client.user.tag)
+})
